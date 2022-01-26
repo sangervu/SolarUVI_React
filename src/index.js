f31190b35d533520893bcb633c7cd7fc072fdff1
Sakari Angervuori <sakari.angervuori@cgi.com>
2022-01-26 15:33:20 +0200

    refact.

--- src/index.js
+++ src/index.js
@@ -6,47 +6,56 @@ import reportWebVitals from './reportWebVitals';
 
 
 const myWebPage = (
-  <div>
-    <div id="demo1"></div>
-    <div id="demo2"></div>
-    <div id="demo3"></div>
-    <div id="demo4"></div>
-    <div id="demo5"></div>
-    <div id="demo6"></div>
-    <div id="demo7"></div>
-    <div id="demo8"></div>
-    <div id="demo9"></div>
-    <div id="demo10"></div>
-    <div id="demo11"></div>
-    <div id="demo12"></div>
-    <div id="demo13"></div>
-    <div id="demo14"></div>
-    <div id="demo15"></div>
-    <div id="demo16"></div>
-    <div id="demo17"></div>
-    <div id="demo18"></div>
-    <div id="demo19"></div>
-    <div id="demo20"></div>
-    <div id="demo21"></div>
-    <div id="demo22"></div>
-    <div id="demo23"></div>
-    <div id="demo24"></div>
+  <>
+    <p id="demo1"></p>
+    <p id="demo2"></p>
+    <p id="demo3"></p>
+    <p id="demo4"></p>
+    <p id="demo5"></p>
+    <p id="demo6"></p>
+    <p id="demo7"></p>
+    <p id="demo8"></p>
+    <p id="demo9"></p>
+    <p id="demo10"></p>
+    <p id="demo11"></p>
+    <p id="demo12"></p>
+    <p id="demo13"></p>
+    <p id="demo14"></p>
+    <p id="demo15"></p>
+    <p id="demo16"></p>
+    <p id="demo17"></p>
+    <p id="demo18"></p>
+    <p id="demo19"></p>
+    <p id="demo20"></p>
+    <p id="demo21"></p>
+    <p id="demo22"></p>
+    <p id="demo23"></p>
+    <p id="demo24"></p>
 
-    <br />
-    <input type="text" id="uviWarning"></input>
-    <br />
-    <input type="date" id="cal" name="trip-start" value="2022-01-01" min="2022-01-01" max="2022-12-31"></input>
-    <button id="get-date">New Date</button>
-    <br />
+    <div>
+      <input type="text" id="uviWarning"></input>
+    </div>
 
-    <button id="find-me">Current location</button>
-    <button id="new-location">New location</button>
-    <label for="fname">latitude:</label>
-    <input type="number" id="lat" value="60.2"></input>
-    <label for="fname">longitude:</label>
-    <input type="number" id="lon" value="24.90"></input>
+    <div>
+      <input type="date" id="cal" name="trip-start" value="2022-01-01" min="2022-01-01" max="2022-12-31"></input>
+      <button id="get-date">New Date</button>
+    </div>
+    <div>
+      <button id="find-me">Current location</button>
+    </div>
+    <div>
+      <button id="new-location">New location</button>
+    </div>
+    <div>
+      <label for="fname">latitude:</label>
+      <input type="number" id="lat" value="60.2"></input>
+    </div>
+    <div>
+      <label for="fname">longitude:</label>
+      <input type="number" id="lon" value="24.90"></input>
+    </div>
     <a id="map-link" target="_blank"></a>
-  </div>);
+  </>);
 
 ReactDOM.render(
   // <React.StrictMode>
