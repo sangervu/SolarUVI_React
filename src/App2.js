bd678627d30d592120f5909adae986a363a4e5f6
Sakari Angervuori <sakari.angervuori@cgi.com>
2022-02-14 09:39:18 +0200

    selitetekstit mukaan

--- src/App2.js
+++ src/App2.js
@@ -11,33 +11,33 @@ function App2() {
     return (
         <>
             <h1>Solar calculations</h1>
-            <p>{app.currentSunElevation}</p>
-            <p>{app.azimuthNSEW}</p>
-            <p>{app.currentSunAzimuth}</p>
-            <p>{app.dayLength}</p>
-            <p>{app.latitudePolarNight}</p>
-            <p>{app.maxSunElevation}</p>
-            <p>{app.solarPower}</p>
-            <p>{app.solarPower}</p>
-            <p>{app.solarPowerMax}</p>
-            <p>{app.solarPowerMaxAnnual}</p>
-            <p>{app.timeRize}</p>
-            <p>{app.timeRizeAstronomical}</p>
-            <p>{app.timeRizeCivil}</p>
-            <p>{app.timeSetNautical}</p>
-            <p>{app.timeSet}</p>
-            <p>{app.timeSetAstronomical}</p>
-            <p>{app.timeSetCivil}</p>
-            <p>{app.timeSetNautical}</p>
-            <p>{app.timeSunSouth}</p>
-            <p>{app.uvIndex}</p>
-            <p>{app.uvIndexEnd}</p>
-            <p>{app.uvIndexMax}</p>
-            <p>{app.uvIndexMaxAnnual}</p>
-            <p>{app.uvIndexOverThree}</p>
-            <p>{app.uvIndexWarning.backgroundColor}</p>
-            <p>{app.uvIndexWarning.color}</p>
-            <p>{app.uvIndexWarning.value}</p>
+            <p>{"currentSunElevation: "} {app.currentSunElevation}</p>
+            <p>{"azimuthNSEW: "} {app.azimuthNSEW}</p>
+            <p>{"currentSunAzimuth: "} {app.currentSunAzimuth}</p>
+            <p>{"dayLength: "} {app.dayLength}</p>
+            <p>{"latitudePolarNight: "} {app.latitudePolarNight}</p>
+            <p>{"maxSunElevation: "} {app.maxSunElevation}</p>
+            <p>{"solarPower: "} {app.solarPower}</p>
+            <p>{"solarPowerAquired: "} {app.solarPowerAquired}</p>
+            <p>{"solarPowerMax: "} {app.solarPowerMax}</p>
+            <p>{"solarPowerMaxAnnual: "} {app.solarPowerMaxAnnual}</p>
+            <p>{"timeRize: "} {app.timeRize}</p>
+            <p>{"timeRizeAstronomical: "} {app.timeRizeAstronomical}</p>
+            <p>{"timeRizeCivil: "} {app.timeRizeCivil}</p>
+            <p>{"timeSetNautical: "} {app.timeSetNautical}</p>
+            <p>{"timeSet: "} {app.timeSet}</p>
+            <p>{"timeSetAstronomical: "} {app.timeSetAstronomical}</p>
+            <p>{"timeSetCivil: "} {app.timeSetCivil}</p>
+            <p>{"timeSetNautical: "} {app.timeSetNautical}</p>
+            <p>{"timeSunSouth: "} {app.timeSunSouth}</p>
+            <p>{"uvIndex: "} {app.uvIndex}</p>
+            <p>{"uvIndexEnd: "} {app.uvIndexEnd}</p>
+            <p>{"uvIndexMax: "} {app.uvIndexMax}</p>
+            <p>{"uvIndexMaxAnnual: "} {app.uvIndexMaxAnnual}</p>
+            <p>{"uvIndexOverThree: "} {app.uvIndexOverThree}</p>
+            <p>{"uvIndexWarning.backgroundColor: "} {app.uvIndexWarning.backgroundColor}</p>
+            <p>{"uvIndexWarning.color: "} {app.uvIndexWarning.color}</p>
+            <p>{"uvIndexWarning.value: "} {app.uvIndexWarning.value}</p>
         </>
     )
 }
