e94adf90881163b1e149d2132cbdd5473ce0481b
Sakari Angervuori <sakari.angervuori@cgi.com>
2022-03-09 09:42:39 +0200

    refaktorointi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react'
+import {useState} from 'react'
 import AppCalendar from './AppCalendar';
 import MapMaker from './MapMakers/MapMaker';
 import Container from 'react-bootstrap/Container';
--- src/SolarResultsUVI.js
+++ src/SolarResultsUVI.js
@@ -1,4 +1,4 @@
-import { useState} from "react";
+import {useState} from "react";
 import React from 'react';
 import './styles/style.css';
 import './styles/App.css';
