f0e08dfc8b087b294a6747f82f319bc72e0ea895
Sakari Angervuori <sakari.angervuori@kotiposti.net>
2022-05-11 22:49:45 +0300

    slider

--- src/main-page/Slider.js
+++ src/main-page/Slider.js
@@ -5,9 +5,15 @@ import './slider.css';
 const Slider = () => {
   return (
     <ReactSlider
-      className="horizontal-slider"
-      thumbClassName="example-thumb"
-      trackClassName="example-track"
+        ariaLabelledby="slider-label"
+        className="horizontal-slider"
+        thumbClassName="example-thumb"
+        trackClassName="example-track"
+        min={0}
+        max={24}
+        defaultValue={12}
+        step={0.1}
+        renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
     />
   );
 };
